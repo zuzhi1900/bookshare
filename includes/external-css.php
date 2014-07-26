85036f92059f40879764a7bd05f3b9ee512b38c2
zuzhi1900 <legacylies@gmail.com>
2014-07-26 10:37:38 +0800

    Revert "adjust the content structure"
    This reverts commit f10d39eb4d1bd42635cd812c7e17a425e234d385.

--- includes/external-css.php
+++ includes/external-css.php
@@ -2,6 +2,6 @@
 <link href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 
 <!-- Custom styles for this template -->
-<link href="/assets/css/jumbotron.css" rel="stylesheet">
+<link href="css/jumbotron.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/bookshare.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
